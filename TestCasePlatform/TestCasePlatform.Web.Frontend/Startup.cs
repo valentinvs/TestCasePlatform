a319e331bc49d4034471448b62238b464aa45fa6
valentinvs <valentin_vs@hotmail.com>
2014-10-19 13:32:24 +0300

    TestCasePlatform: Solution and projects created

---
+++ TestCasePlatform/TestCasePlatform.Web.Frontend/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestCasePlatform.Web.Frontend.Startup))]
+namespace TestCasePlatform.Web.Frontend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ TestCasePlatform/TestCasePlatform/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCasePlatform
+{
+    public class Class1
+    {
+    }
+}
