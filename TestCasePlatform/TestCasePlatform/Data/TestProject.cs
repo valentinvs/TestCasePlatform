4a6e8628906b679db1b921e55da678698f31a36b
valentinvs <valentin_vs@hotmail.com>
2014-10-19 23:18:21 +0300

    TestCasePlatform: Added entity TestProject.cs to source control

---
+++ TestCasePlatform/TestCasePlatform/Data/TestProject.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestCasePlatform.Data
+{
+    public class TestProject
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(256)]
+        public string Title { get; set; }
+
+        [StringLength(1024)]
+        public string Description { get; set; }
+    }
+}
